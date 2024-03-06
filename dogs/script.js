f7bae768f795699f581d24db87164bdb1e035c51
Runquan (Ray) Zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-03-06 01:41:58 -0500

    add amber profile

--- dogs/script.js
+++ dogs/script.js
@@ -2,4 +2,38 @@ const ele = document.createElement("span")
 
 document.querySelector("h1").append(ele)
 
-document.querySelector("span").innerText = "They're all good dogs!"
+document.querySelector("span").innerText = "(They're all good dogs!)"
+
+ele.classList.add("underline")
+
+const amberArticle = document.createElement("article")
+
+const image = document.createElement("img")
+
+image.setAttribute(
+    "src",
+    "https://placedog.net/500/450"
+)
+
+image.setAttribute(
+    "alt",
+    "Amber, the dog."
+)
+
+image.classList.add("animated")
+
+const amberName = document.createElement("h2")
+
+const amberDescriptionP = document.createElement("p")
+
+document.querySelector("main").append(amberArticle)
+
+document.querySelector("article").append(image)
+
+document.querySelector("article").append(amberName)
+
+document.querySelector("h2").innerText = "Amber"
+
+document.querySelector("article").append(amberDescriptionP)
+
+document.querySelector("p").innerText = "A sweet little doggo that belongs to my friend."
